39ec63e07578309caa4693d02b1de715caf0404a
Mattias Waldo <mattias.waldo@saabgroup.com>
2021-07-01 09:10:22 +0200

    Actually include all given packets in clean.

--- clean.sh
+++ clean.sh
@@ -1,1 +1,2 @@
-docker run --rm -it -v $PWD/work:/home/yoctouser/work sel4_vm_build /bin/bash -c "source /home/yoctouser/work/oe-init-build-env && bitbake -c cleanall $@"
+PACKETS="$*"
+docker run --rm -it -v $PWD/work:/home/yoctouser/work sel4_vm_build /bin/bash -c "source /home/yoctouser/work/oe-init-build-env && bitbake -c cleanall $PACKETS"
