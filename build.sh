5d3438019d4cdd943ce9c7794576686a9d543f22
Olof Holmberg <oloho254@student.liu.se>
2021-10-07 15:11:54 +0200

    updated revision for meta vis

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ git -C work/meta-cloud-services checkout d33dd7566e3d5e28d250354bbbee9af7350734f
 git -C work clone -b hardknott git://git.openembedded.org/meta-openembedded         2>/dev/null
 git -C work/meta-openembedded checkout aca88908fd329f5cef6f19995b072397fb2d8ec6
 git -C work clone -b hardknott git://git.yoctoproject.org/meta-virtualization       2>/dev/null
-git -C work/meta-virtualization checkout d33dd7566e3d5e28d250354bbbee9af7350734f9
+git -C work/meta-virtualization checkout 3508b13acbf669a5169fafca232a5c4ee705dd16
 cd work
 . oe-init-build-env
 
